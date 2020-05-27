f725a412fdb0f6c915a39d572afb145565f29090
shirishagajam <64522203+shirishagajam@users.noreply.github.com>
2020-05-27 16:44:24 +0530

    Update Box.js
    Some changes in the constructor

--- Box.js
+++ Box.js
@@ -1,15 +1,18 @@
 class Box{
      constructor(x,y,width,height){
-        var potins = {
-            isStatic = true,
+        var position = {
+            isStatic : true
         }
-        this.body = bodies.rectangle(x,y,width,height);
+        this.body = Bodies.rectangle(x,y,width,height,position);
+        
+        this.width = width;
+        this.height = height;
         World.add(world,this.body);
     }
 
         display(){
             var pos = this.body.position;
-            rectMoad(CENTER);
+            rectMode(CENTER);
             fill(255);
             rect(pos.x,pos.y,this.width,this.height);
         }
