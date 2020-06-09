4a2d38bb2e74d120e12659038f536389633ccc83
darshagarwal <64535202+darshagarwal@users.noreply.github.com>
2020-06-09 22:22:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,11 +3,8 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 var box1,box2,box3;
-var triangle1;
 var box4,box5;
-var triangle2;
 var box6;
-var triangle3;
 var box7;
 
 function setup() {
@@ -18,12 +15,9 @@ function setup() {
   box1 = new Box(700,300,20,500);
   box2 = new Box(630,300,75,220);
   box3 = new Box(555,325,75,175);
-  triangle1 = new Box(630,175,75,50);
   box4 = new Box(468,325,100,150);
   box5 = new Box(380,325,75,175);
-  triangle2 = new Box(470,200,90,100);
   box6 = new Box(308,300,75,200);
-  triangle3 = new Box(308,175,75,50);
   box7 = new Box(240,300,20,500);
 
 
